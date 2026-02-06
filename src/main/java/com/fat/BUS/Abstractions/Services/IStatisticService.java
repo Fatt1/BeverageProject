528176189db8376018acedfb6e692c3385d190a4
MynameNghia123 <nghialam1509@gmail.com>
2026-02-06 17:09:02 +0700

    Revenue

--- src/main/java/com/fat/BUS/Abstractions/Services/IStatisticService.java
+++ src/main/java/com/fat/BUS/Abstractions/Services/IStatisticService.java
@@ -1,5 +1,6 @@
 package com.fat.BUS.Abstractions.Services;
 
+import com.fat.DTO.Statistics.RevenueDTO;
 import com.fat.DTO.Statistics.StockStatisticDTO;
 
 import java.time.LocalDate;
@@ -7,5 +8,9 @@ import java.util.List;
 
 public interface IStatisticService {
     List<StockStatisticDTO> getStockStatistic(LocalDate fromDate, LocalDate toDate);
+    List<RevenueDTO> getRevenueStatisticsByMonth(int year);
+    List<RevenueDTO> getRevenueStatisticsByYear(int startYear, int endYear);
+    List<RevenueDTO> getRevenueStatisticsByDay(int year, int month);
+    List<RevenueDTO> getRevenueStatisticsByQuarter(int year, int quarter);
 
 }
